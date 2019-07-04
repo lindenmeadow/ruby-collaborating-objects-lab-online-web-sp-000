cac6331ce249fe958b3f1c2c73f0c1fcd011696b
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-04 01:16:46 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,13 @@
 class MP3Importer
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files 
+    @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
+  end
+  
   def import
     self.files.each{ |filename| Song.new_by_filename(filename) }
   end
