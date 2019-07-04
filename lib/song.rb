18cc2e04695682477fd09b12ca4cadc828021173
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-04 01:19:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :name, :Artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+    song.artist_name = artist
+    song
+  end
+end
